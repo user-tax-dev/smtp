78133e7a950f281df713699a762d0840fa002205
any <any@user.tax>
2022-09-09 17:11:44 +0800

    　

---
+++ gen.DKIM.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+DIR=$(dirname $(realpath "$0"))
+cd $DIR
+set -ex
+
+cd ../key/smtp
+host=user.tax
+openssl genrsa -out $host.pem 2048
+
+
+
+txt=$(openssl rsa -in $host.pem -pubout -outform der 2>/dev/null | openssl base64 -A)
+
+year=$(date "+%Y")
+
+echo -e "\nset txt\n$year._domainkey.$host"
+echo -e "v=DKIM1; k=rsa; p=$txt"
+
+
