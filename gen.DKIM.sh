d228876d19357d578b72fc10007eca8d5d13b04b
any <any@user.tax>
2022-09-09 17:12:02 +0800

    　

--- gen.DKIM.sh
+++ gen.DKIM.sh
@@ -2,13 +2,10 @@
 
 DIR=$(dirname $(realpath "$0"))
 cd $DIR
-set -ex
 
 cd ../key/smtp
 host=user.tax
 openssl genrsa -out $host.pem 2048
-
-
 
 txt=$(openssl rsa -in $host.pem -pubout -outform der 2>/dev/null | openssl base64 -A)
 
